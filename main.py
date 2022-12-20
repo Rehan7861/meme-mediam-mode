750e83cb67edff1cd37e634b9b461dd592516dfb
Rehan Mohammed <>
2022-12-20 21:26:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+lst[]
+riceboyyymaxiq = 0
+riceboyyyiq = int(input("How is weewan so dumb? "))
+for i in range (riceboyyyiq):
+  riceboyyyiq2 = int(input("Why is he such a little numpty? "))
+  lst.append(riceboyyyiq2)
+print(lst)
+
+for 
